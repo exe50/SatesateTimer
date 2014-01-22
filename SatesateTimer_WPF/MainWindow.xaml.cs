d3123f6a99f2d515d56be3e254846f9933d23766
exe50 <exe50@hotmail.com>
2014-01-22 12:57:17 +0900

    test

---
+++ SatesateTimer_WPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Configuration;
+using TweetSharp;
+
+
+
+namespace SatesateTimer_WPF
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static public TwitterService service = new TwitterService("0KVaGxWPhXfPs8Tm42Pdhg", "nL3dnrnVaZ4F6ZxK6SI7z2kYTSfzxi3pSHoxoqRXw");
+        static public OAuthRequestToken requestToken = service.GetRequestToken();
+        static public Uri uri = service.GetAuthenticationUrl(requestToken);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        #region ConsumerKey & ConsumerSecret
+        private static string ConsumerSecret
+        {
+            get { return ConfigurationManager.AppSettings["ConsumerSecret"]; }
+        }
+
+        private static string ConsumerKey
+        {
+            get { return ConfigurationManager.AppSettings["ConsumerKey"]; }
+        }
+        #endregion
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        public void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+                //if (SharedState.Authorizer == null)
+               new OAuth().Show();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SatesateTimer_WPF/OAuth.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TweetSharp;
+using System.Diagnostics;
+
+namespace SatesateTimer_WPF
+{
+    public partial class OAuth : Window
+    {
+
+        public OAuth()
+        {
+            InitializeComponent();
+
+            
+            //Process.Start(uri.ToString());
+            OAuthWebBrowser.Source = MainWindow.uri;
+            //string verifier = Console.ReadLine();
+
+            //OAuthAccessToken access = service.GetAccessToken(requestToken, verifier);
+            //service.AuthenticateWith(access.Token, access.TokenSecret);
+
+        }
+
+    }
+}
